cec3fd4cf4f6da3cfb1a3379541bbc907fda6bfb
bhargavtz <bhargav05@yandex.com>
2025-03-09 00:19:07 +0530

    update datalog

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
